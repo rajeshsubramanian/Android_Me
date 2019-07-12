837fddd5cc4edc3b1af5b18e5663f70aa237bae8
Rajesh Subramanian <rsubramanian@resourceammirati.com>
2019-07-12 14:00:35 -0400

    Commit for exercice 1

---
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -0,0 +1,28 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.AppCompatImageView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
+
+public class BodyPartFragment extends Fragment {
+
+	public BodyPartFragment() {
+	}
+
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+		View view = inflater.inflate(R.layout.fragment_body_part, container, false);
+		AppCompatImageView bodyPartImageView = (AppCompatImageView) view.findViewById(R.id.bodyPartImageView);
+		bodyPartImageView.setImageResource(AndroidImageAssets.getHeads().get(0));
+		return view;
+	}
+}
